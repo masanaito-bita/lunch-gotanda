4dd3b185bb47f56cb30414fd1c29d1b63bf9eaf3
masanaito-bita <masana_ito@bita.jp>
2022-06-20 22:37:53 +0900

    feat: responseを送りjsonで検索結果を受け取る

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-const apiKey = '244f51358beb91f3';
+const API_KEY = '244f51358beb91f3';
+const AREA_GOTANDA = 'small_area=X086'
+const FORMAT = 'format=json';
+const IS_LUNCH = 'lunch=1';
+const URL = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&${FORMAT}&${AREA_GOTANDA}&${IS_LUNCH}`;
+
+/**
+ * hotpepper
+ */
+const search = () => {
+  fetch(URL).then(response => {
+    console.log(response.status);
+    return response.json().then(function (results) {
+        console.log(results);
+      });
+  });
+}
+
+const searchButton = document.querySelector('.js-search');
+searchButton.addEventListener('click', search);
+
+search();
