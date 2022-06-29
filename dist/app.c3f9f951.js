e0717d65852a975ee0083e7521b21eeb861dd01d
masanaito-bita <masana_ito@bita.jp>
2022-06-29 16:15:41 +0900

    style: cssファイルの指定を変更

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -381,7 +381,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59904" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52888" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
